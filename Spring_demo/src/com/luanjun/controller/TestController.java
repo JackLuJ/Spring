ec91545685dba0c17a78c0a0acf71644a6d3c3f0
JackLuJ <1183217863@qq.com>
2018-06-21 16:52:16 +0800

    配置文件修改

--- Spring_demo/src/com/luanjun/controller/TestController.java
+++ Spring_demo/src/com/luanjun/controller/TestController.java
@@ -1,7 +1,17 @@
 package com.luanjun.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.luanjun.base.BaseController;
 
+@Controller
 public class TestController extends BaseController {
 
+	@RequestMapping(value="/test.do", method=RequestMethod.GET)
+	public String test(@RequestParam("world") String world){
+		return "Hello "+world;
+	}
 }
