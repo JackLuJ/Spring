f159dee6112ca70c6a9e84acedab0422847050f3
JackLuJ <1183217863@qq.com>
2018-07-09 16:24:32 +0800

    freemarker添加

---
+++ Spring_demo/WebContent/WEB-INF/ftl/may.ftl
@@ -0,0 +1,8 @@
+ser:<br/>
+${user.name}--->${user.age}<br/>
+List:<br/>
+<#list list as item >
+<font color="red">${item}</br></font>
+</#list>
+param:</br>
+${RequestParameters.a}&nbsp;a=${param!}
--- Spring_demo/src/com/luanjun/controller/TestController.java
+++ Spring_demo/src/com/luanjun/controller/TestController.java
@@ -1,17 +1,37 @@
 package com.luanjun.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.luanjun.base.BaseController;
 
 @Controller
 public class TestController extends BaseController {
 
-	@RequestMapping(value="/test.do", method=RequestMethod.GET)
-	public String test(@RequestParam("world") String world){
-		return "Hello "+world;
+	@RequestMapping(value = "/test.do", method = RequestMethod.GET)
+	public ModelAndView test(@RequestParam(value = "a", required = false) String a) {
+		ModelAndView mv = new ModelAndView();
+		Map<String, Object> user = new HashMap<String, Object>();
+		user.put("name", "Jack.LuJ");
+		user.put("age", 26);
+		
+		List<String> list = new ArrayList<String>();
+		list.add("Jack1");
+		list.add("Jack2");
+		list.add("Jack3");
+
+		mv.addObject("list", list);
+		mv.addObject("user", user);
+		mv.addObject("param", a);
+		mv.setViewName("may");
+		return mv;
 	}
 }
